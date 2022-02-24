6c36dafbfd2e46a153e1dd13f0c2e5ed1c976989
Ingole987 <pratik.ingole98@gmail.com>
2022-02-24 14:50:30 +0530

    Added: Additional info like Address,Dept,Phone

---
+++ ERDiagram.sql
@@ -0,0 +1,20 @@
+use Employee_Payroll_Services
+
+select * from employee_payroll
+
+alter table employee_payroll add Phone varchar(250);
+alter table employee_payroll add address varchar (250);
+alter table employee_payroll add department varchar(250);
+
+select * from employee_payroll
+
+alter table employee_payroll add
+constraint DefaultDepartment_constraint default 'CS' for department
+
+select * from employee_payroll
+
+alter table employee_payroll add
+constraint DefaultAddress_constraint default 'Mumbai' for address
+
+select * from employee_payroll
+
