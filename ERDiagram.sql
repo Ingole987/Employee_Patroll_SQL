ff7f7aac7f9053bae1286699e621ebacb9704ad5
Ingole987 <pratik.ingole98@gmail.com>
2022-02-24 15:02:01 +0530

    Feat: updated Addresses,Department etc.

--- ERDiagram.sql
+++ ERDiagram.sql
@@ -25,6 +25,20 @@ alter table employee_payroll add net real;
 
 select * from employee_payroll
 
+sp_rename 'employee_payroll.salary','basic_pay'
+
+select * from employee_payroll
+
+update employee_payroll set  department= 'marketing', Phone = 7895646531, address = '1652 passion street' where name = 'Billi';
+update employee_payroll set  department= 'sales', Phone = 7895315531, address = '1652 Grand Walk street' where name = 'Terisa';
+update employee_payroll set  department= 'marketing', Phone = 7895646353, address = '1652 passion street' where name = 'Charlie';
+update employee_payroll set  department= 'HR', Phone = 6131846313, address = '2057  Spadafore Drive' where name = 'Mark';
+update employee_payroll set department= 'quality', Phone = 5913564986, address = '153  Clarksburg Park Road' where name = 'Terisa';
+
+select*from employee_payroll
+
+
+
 
 
 
