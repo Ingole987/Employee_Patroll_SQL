0a907fc4c269773fa0154d47ddbbe4c2504e8a19
Ingole987 <pratik.ingole98@gmail.com>
2022-02-24 14:00:00 +0530

    feat: added method to create table

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -4,3 +4,12 @@ use Employee_Payroll_Services
 
 select DB_NAME();
 
+create table employee_payroll
+(
+ id int identity(1,1) primary key,
+ name varchar(100) not null,
+ salary money not null,
+ start date not null
+);
+
+select * from employee_payroll;
